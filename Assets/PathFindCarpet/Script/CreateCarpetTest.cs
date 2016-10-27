825674205544c989151e032c5af7757bbafc51bd
lolicon77 <huangforbusi@foxmail.com>
2016-10-27 11:13:07 +0800

    ~

---
+++ Assets/PathFindCarpet/Script/CreateCarpetTest.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using Test;
+using UnityEngine;
+
+public class CreateCarpetTest : MonoBehaviour {
+
+	public Material material;
+	public Vector3[] verts;
+
+
+	void OnGUI() {
+		if (GUILayout.Button("test")) {
+			CreateCarpetByVerts(0.2f);
+		}
+	}
+
+	void CreateCarpetByVerts(float width) {
+		List<Mesh> meshList = new List<Mesh>();
+		for (int i = 0; i < verts.Length - 1; i++) {
+			meshList.Add(CreateMeshByTwoPoint(verts[i], verts[i + 1], width));
+		}
+		Mesh mesh = new Mesh();
+		CombineInstance[] combine = new CombineInstance[meshList.Count];
+		for (int i = 0; i < meshList.Count; i++) {
+			combine[i].mesh = meshList[i];
+			combine[i].transform = Matrix4x4.identity;
+		}
+		mesh.CombineMeshes(combine);
+		gameObject.AddComponent<MeshFilter>().mesh = mesh;
+		gameObject.AddComponent<MeshRenderer>().material = material;
+		UVMove.AddUVMoveComponent(gameObject, 3);
+		//		CombineMesh();
+	}
+
+	Mesh CreateMeshByTwoPoint(Vector3 start, Vector3 end, float width) {
+		float halfWidth = width * 0.5f;
+		var deltaVert = end - start;
+		float length = Vector3.Magnitude(deltaVert);
+		float lengthWidthRatio = length / width;
+		var dir = Vector3.Cross(Vector3.Normalize(deltaVert), Vector3.up);
+		Mesh mesh = new Mesh {
+			vertices = new Vector3[]
+			{
+				start + dir*halfWidth,
+				start - dir*halfWidth,
+				end + dir*halfWidth,
+				end - dir*halfWidth,
+			},
+			triangles = new int[]
+			{
+				0, 2, 1, 1, 2, 3
+			},
+			uv = new[]
+			{
+				Vector2.zero,
+				Vector2.up,
+				Vector2.right *lengthWidthRatio,
+				Vector2.right *lengthWidthRatio +Vector2.up,
+			}
+		};
+		return mesh;
+		//		InstantiateNewChildMesh(mesh);
+	}
+
+	void InstantiateNewChildMesh(Mesh mesh) {
+		var child = new GameObject("test");
+		child.AddComponent<MeshRenderer>().material = material;
+		child.AddComponent<MeshFilter>().mesh = mesh;
+		child.transform.SetParent(transform);
+		child.transform.localPosition = Vector3.zero;
+		child.transform.localRotation = Quaternion.identity;
+		child.transform.localScale = Vector3.one;
+	}
+
+	void CombineMesh() {
+		MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+		CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+		int i = 0;
+		while (i < meshFilters.Length) {
+			combine[i].mesh = meshFilters[i].sharedMesh;
+			combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+			meshFilters[i].gameObject.SetActive(false);
+			i++;
+		}
+		gameObject.AddComponent<MeshFilter>().mesh = new Mesh();
+		transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+		gameObject.AddComponent<MeshRenderer>().material = material;
+		gameObject.SetActive(true);
+	}
+
+}
---
+++ Assets/PathFindCarpet/Script/UVMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Test {
+	public class UVMove : MonoBehaviour {
+
+		public float speed;
+		public Material mat;
+
+		public static void AddUVMoveComponent(GameObject go, float speed, Material mat = null) {
+			var component = go.AddComponent<UVMove>();
+			component.speed = speed;
+			component.mat = mat;
+		}
+
+		void Start() {
+			if (!mat) {
+				mat = GetComponent<Renderer>().sharedMaterial;
+			}
+		}
+
+		void Update() {
+			mat.mainTextureOffset = new Vector2(1 - Time.time % 1 * speed, 0);
+		}
+	}
+}
